7d726729fb4d69b4fb593ce0b163ac65fed8017e
mjohnson <matthewj301@gmail.com>
2022-10-21 21:45:28 -0500

    saving section20 challenge2

--- section20-challenge1/main.cpp
+++ section20-challenge1/main.cpp
@@ -8,24 +8,60 @@
 #include <vector>
 #include <iomanip>
 
-bool is_palindrome(const std::string& s)
-{
+bool is_palindrome(const std::string &s) {
     // You must implement this function.
     // Since we are learning the STL - use a deque to solve the problem.
+    std::deque<char> forward;
+    std::deque<char> reverse;
+    for (auto letter: s) {
+        if (isalpha(letter)) {
+            forward.push_back(tolower(letter));
+            reverse.push_front(tolower(letter));
+        }
+    }
+    if (forward == reverse) {
+        return true;
+    }
     return false;
 }
 
-int main()
-{
-    std::vector<std::string> test_strings{ "a", "aa", "aba", "abba", "abbcbba", "ab", "abc", "radar", "bob", "ana",
-        "avid diva", "Amore, Roma", "A Toyota's a toyota", "A Santa at NASA", "C++",
-        "A man, a plan, a cat, a ham, a yak, a yam, a hat, a canal-Panama!", "This is a palindrome", "palindrome" };
-   
+bool is_another_palindrome(const std::string &s) {
+    // You must implement this function.
+    // Since we are learning the STL - use a deque to solve the problem.
+    std::deque<char> reverse;
+    for (auto letter: s) {
+        if (isalpha(letter)) {
+            reverse.push_front(std::tolower(letter));
+        }
+    }
+    char f_char{};
+    char b_char{};
+    while (reverse.size() > 1) {
+        f_char = reverse.front();
+        b_char = reverse.back();
+        if (f_char != b_char) {
+            return false;
+        }
+        reverse.pop_front();
+        reverse.pop_back();
+    }
+    return true;
+}
+
+int main() {
+    std::vector<std::string> test_strings{"a", "aa", "aba", "abba", "abbcbba", "ab", "abc", "radar", "bob", "ana",
+                                          "avid diva", "Amore, Roma", "A Toyota's a toyota", "A Santa at NASA", "C++",
+                                          "A man, a plan, a cat, a ham, a yak, a yam, a hat, a canal-Panama!",
+                                          "This is a palindrome", "palindrome"};
+
     std::cout << std::boolalpha;
     std::cout << std::setw(8) << std::left << "Result" << "String" << std::endl;
-    for(const auto& s : test_strings) {
-        std::cout << std::setw(8) << std::left << is_palindrome(s)  << s << std::endl;
+    for (const auto &s: test_strings) {
+        // My solution
+        std::cout << std::setw(8) << std::left << is_palindrome(s) << " " << s << std::endl;
+        // Instructor solution
+        std::cout << std::setw(8) << std::left << is_another_palindrome(s) << " " << s << std::endl;
+        std::cout << std::endl;
     }
-    std::cout << std::endl;
     return 0;
 }
--- section20-challenge2/main.cpp
+++ section20-challenge2/main.cpp
@@ -8,6 +8,7 @@
 #include <cctype>
 #include <iomanip>
 #include <limits>
+#include <limits>
 
 class Song {
     friend std::ostream &operator<<(std::ostream &os, const Song &s);
@@ -27,7 +28,6 @@ public:
     int get_rating() const {
         return rating;
     }
-    
     bool operator<(const Song &rhs) const  {
         return this->name < rhs.name;
     }
@@ -55,17 +55,20 @@ void display_menu() {
 }
 
 void play_current_song(const Song &song) {
-    // This function should display 
-    // Playing: followed by the song that is playing
-   
-    std::cout << "You implement this function"<< std::endl;
+    std::cout << std::left << "Playing:" << std::endl;
+    std::cout << song << std::endl;
+    //std::cout << std::setw(21) << std::left << song.get_name() <<  std::setw(30) << song.get_artist() << std::endl;
 }
 
 void display_playlist(const std::list<Song> &playlist, const Song &current_song) {
-    // This function should display the current playlist 
-    // and then the current song playing.
-    
-    std::cout << "You implement this function" << std::endl;
+    std::cout << std::setw(20) << std::left << "Name"
+              << std::setw(30) << std::left << "Artist"
+              << std::setw(2) << std::left << "Rating" << std::endl;
+    for (const auto& song: playlist) {
+        std::cout << song << std::endl;
+    }
+    std::cout << std::endl << "Current Song" << std::endl;
+    std::cout << current_song << std::endl;
 }
 
 int main() {
@@ -74,15 +77,65 @@ int main() {
             {"God's Plan",        "Drake",                     5},
             {"Never Be The Same", "Camila Cabello",            5},
             {"Pray For Me",       "The Weekend and K. Lamar",  4},
-            {"The Middle",        "Zedd, Maren Morris & Grey", 5},
-            {"Wait",              "Maroone 5",                 4},
-            {"Whatever It Takes", "Imagine Dragons",           3}          
+            //{"The Middle",        "Zedd, Maren Morris & Grey", 5},
+            //{"Wait",              "Maroone 5",                 4},
+            //{"Whatever It Takes", "Imagine Dragons",           3}
     };
     
     std::list<Song>::iterator current_song = playlist.begin();
     
     std::cout << "To be implemented" << std::endl;
-    // Your program logic goes here
+    char user_selection {};
+    std::string new_song_name, new_artist;
+    int new_rating;
+    while (user_selection != 'q') {
+        display_menu();
+        std::cin >> user_selection;
+        std::cin.clear();
+        switch (user_selection) {
+            case 'f':
+                std::cout << 'Playing first song' << std::endl;
+                current_song = playlist.begin();
+                play_current_song(*current_song);
+                break;
+            case 'n':
+                std::cout << "Playing next song" << std::endl;
+                current_song++;
+                if (current_song == playlist.end()) {
+                    current_song = playlist.begin();
+                }
+                play_current_song(*current_song);
+                break;
+            case 'p':
+                std::cout << "Playing previous song" << std::endl;
+                if (current_song == playlist.begin()) {
+                    current_song = playlist.end();
+                }
+                current_song--;
+                play_current_song(*current_song);
+                break;
+            case 'a':
+                std::cin.clear();
+                std::cin.ignore(std::numeric_limits<std::streamsize>::max(),'\n');
+                std::cout << "Enter a song name: ";
+                std::getline(std::cin, new_song_name);
+                std::cout << "Enter artist: ";
+                std::getline(std::cin, new_artist);
+                std::cout << "Enter your rating";
+                std::cin >> new_rating;
+                playlist.insert(current_song, Song(new_song_name, new_artist, new_rating));
+                current_song--;
+                play_current_song(*current_song);
+                break;
+            case 'l':
+                display_playlist(playlist, *current_song);
+                break;
+            default:
+                std::cout << "Please enter a valid response." << std::endl << std::endl;
+                break;
+
+        }
+    }
 
     std::cout << "Thanks for listening!" << std::endl;
     return 0;
